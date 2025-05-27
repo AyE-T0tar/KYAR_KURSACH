722ed5d6654ff7ef60e97fe9e4b2bd809bbd4161
AyE-T0tar <egorsemenovich07@gmail.com>
2025-05-27 14:57:21 +0300

    Delete Курсовой по КЯР directory

--- Курсовой по КЯР/JS/mobile.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.querySelector('.overlay-header .menu-toggle');
-    const navMenu = document.querySelector('.overlay-header nav');
-    
-    toggleBtn.addEventListener('click', function() {
-      navMenu.classList.toggle('expanded');
-      toggleBtn.classList.toggle('collapsed');
-    });
-  });
